8555730aa98571e7dd910ecf2a411ea35fd5522b
guiglabs <gelabert.guillem+github@gmail.com>
2019-01-28 18:59:02 +0100

    fix: move routes to src

--- src/routes/negotiations.js
+++ src/routes/negotiations.js
@@ -1,11 +1,19 @@
 const express = require('express');
 
 const router = express.Router();
-const mock = require('../__test__/__mock__/negotations.mock');
 
-/* POST negotiation */
-router.post('/', (req, res, next) => {
-  res.status(201);
+router.get('/:negotiationId', (req, res) => {
+  const { negotiationId } = req.params;
+  res.send(`GET request to negotiations/${negotiationId}`);
+  res.sendStatus(201);
+});
+
+router.get('/all', (req, res) => {
+  res.sendStatus(200);
+});
+
+router.post('/create', (req, res) => {
+  res.sendStatus(201);
 });
 
 module.exports = router;
