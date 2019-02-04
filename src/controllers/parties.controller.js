3c2fab57372c4c4b90ef8253f09e8481106cf580
guiglabs <gelabert.guillem+github@gmail.com>
2019-02-04 09:52:29 +0100

    get user user details by email

--- src/controllers/parties.controller.js
+++ src/controllers/parties.controller.js
@@ -3,6 +3,7 @@ class Parties {
     this.model = model;
     this.createParty = this.createParty.bind(this);
     this.getPartyDetails = this.getPartyDetails.bind(this);
+    this.getPartyDetailsByEmail = this.getPartyDetailsByEmail.bind(this);
   }
 
   // eslint-disable-next-line no-unused-vars
@@ -19,6 +20,24 @@ class Parties {
       // eslint-disable-next-line no-console
       console.log(error);
       res.status(422).send('Error creating party :(');
+    }
+  }
+
+  // eslint-disable-next-line no-unused-vars
+  async getPartyDetailsByEmail(req, res, next) {
+    try {
+      const { email } = req.params;
+      const data = await this.model.findOne({ where: { email } });
+      res.status(200).send({
+        legalName: data.legalName,
+        displayName: data.displayName,
+        email: data.email,
+        address: data.address,
+      });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+      res.status(404).send('Error finding email :(');
     }
   }
 
