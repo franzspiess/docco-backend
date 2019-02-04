23528ccc782782d9a8475adfcb6373b6205a95c8
radleylewis <radley.lewis@gmail.com>
2019-02-04 19:39:59 +0100

    feat: npm run bootstrap for database setup

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   development: {
-    host: process.env.DB_HOSTNAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     logging: false, // removes SELECT 1+1 default response on connection
     operatorsAliases: false,
@@ -13,6 +13,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    multipleStatements: true,
   },
   test: {
     dialect: 'mysql',
@@ -24,10 +25,11 @@ module.exports = {
       acquire: 30000,
       idle: 10000,
     },
-    host: process.env.DB_HOSTNAME,
+    host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    multipleStatements: true,
   },
   production: {
     dialect: 'mysql',
@@ -39,9 +41,10 @@ module.exports = {
       acquire: 30000,
       idle: 10000,
     },
-    host: process.env.DB_HOSTNAME,
+    host: process.env.DB_HOST,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
+    multipleStatements: true,
   },
 };
---
+++ src/models/migrations/migrate.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+const path = require('path');
+const mysql = require('mysql');
+const fs = require('fs');
+
+const migrationScript = fs.readFileSync(path.join(__dirname, '/db'), 'utf8');
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  multipleStatements: true,
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  else {
+    // eslint-disable-next-line no-unused-vars
+    connection.query(migrationScript, (error, result, fields) => {
+      if (error) throw error;
+      // eslint-disable-next-line no-console
+      console.log('Database has been created');
+    });
+  }
+  connection.end();
+});
