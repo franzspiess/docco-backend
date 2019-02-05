68c5e3b26d270bd78be515438252329c899fce5e
Edward Neuwirth <edward.neuwirth@outlook.com>
2019-02-05 19:38:29 +0100

    fix: auth middleware to negotiation routes

--- src/routes/negotiations.routes.js
+++ src/routes/negotiations.routes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
+const auth = require('../middleware/auth');
 
 module.exports = (controller) => {
-  router.post('/', controller.create);
-  router.post('/publish/:negotiationId', controller.publish);
-  router.get('/:negotiationId', controller.getOne);
-  router.get('/', controller.getAll);
+  router.post('/', auth, controller.create);
+  router.post('/publish/:negotiationId', auth, controller.publish);
+  router.get('/:negotiationId', auth, controller.getOne);
+  router.get('/', auth, controller.getAll);
   return router;
 };
